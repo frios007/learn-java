63215d7b6af296b5f50a793653b0d0ca1fdabbef
rios09 <fabiorios505@gmail.com>
2024-07-22 21:34:58 -0600

    avance

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // uso de switch
 
-const day ='kjjn';
+/* const day ='kjjn';
 
 switch(day){
     case 'monday':
@@ -38,4 +38,60 @@ if(day === 'monday'){
     console.log('descansamos');
 }else{
     console.log('ningun dia?')
-}
+}  */
+    
+
+// 3+4; // es una expresion por que produce un valor en java
+// 1991 // produce un int o hasta un string
+// true && false && !false // tambien expresion, de tipo boolean    
+
+// // if else  es un enunciado (statment) por que contiene una serie de expresiones que generan
+// // acciones, no produce un valor, sirve para declarar y realizar acciones
+
+// if(23>10){ // <== eso es una expresion
+//     const str = '23 is bigger'/* lo de abajo seria enunciado (statment) */
+//     console.log(str);           
+// }
+
+// const yo = 'fabio' // <== expresion
+// console.log(`tengo ${2024-2000} años y me llamo ${yo}`); //lo que esta en los placeholders son epxresiones, el resto es un enunciado.
+
+
+/*
+    Operador Condicional ternario: se llama asi porque tiene 3 partes, la condicion, el if y el else
+*/
+// ` ` altGr + } para el backtick o comilla invertida para un placeholder ${}
+
+//const edad = 15;
+// edad >=18 ? console.log('Me gusta beber vino'):
+// console.log('Bebo agua');
+
+// con los operadores condicionales ternarios podemos asignar valores a variables si se cumple o no cierta condicion
+
+// const beber = edad>=18 ? 'bamboo': 'agua';
+// console.log(beber);
+
+//comparativa mas larga contra enunciado if
+
+// let beber2;
+// if(edad>=18){
+//     beber2 = 'bamboo';
+// } else{
+//     beber2 = 'agua';
+// }
+// console.log(beber2);
+
+// console.log(`Me gusta beber ${edad>=18 ? 'bamboo': 'agua'}`);
+// el ternario si sirve dentro de un template literal (literal de plantilla)
+//a diferencia de un bloque completo de enunciado If
+// es una forma de crear cadenas de texto que permite incrustar expresiones y
+//variables de manera más fácil y legible en comparación con las cadenas tradicionales.
+
+/* const cuenta = 275;
+const propina = cuenta<= 300 && cuenta>=50? cuenta * 0.15 : cuenta * 0.20;
+
+
+console.log(`La cuenta fue de ${cuenta},
+la propina de ${propina} y el total es de ${cuenta+propina}`); */
+
+
